86af0f8dd3d1de4f0a00a826559425a682b0d25a
Millmoss <omelliff@yahoo.com>
2019-07-30 16:01:19 -0400

    Adding spawnscript

---
+++ Steam Inventors Studio Project/Assets/SpawnScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour
+{
+	public GameObject g;
+	public int i;
+	private float t = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		t += Time.deltaTime;
+
+		if (t > 1)
+		{
+			t = 0;
+			GameObject temp = Instantiate<GameObject>(g);
+			temp.transform.position = new Vector3(0, 5, 0);
+		}
+    }
+}

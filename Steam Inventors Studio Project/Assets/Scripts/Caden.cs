6aef3a3c4bf524109a6a9f52051336894ede5815
Caden <cadendemarco1@gmail.com>
2019-07-31 09:23:54 -0400

    Added movement to Unity Game

---
+++ Steam Inventors Studio Project/Assets/Scripts/Caden.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Caden : MonoBehaviour
+{
+
+    private Rigidbody rb3d;
+    public float speed;
+    void Start()
+    {
+        rb3d = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        rb3d.AddForce(new Vector3(moveVertical, 0, -moveHorizontal));
+        
+        /*
+        if (Input.GetKey(KeyCode.W))
+        {
+            gameObject.transform.position = gameObject.transform.position + new Vector3(1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            gameObject.transform.position = gameObject.transform.position + new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.position = gameObject.transform.position + new Vector3(0, 0, 1);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.position = gameObject.transform.position + new Vector3(0, 0, -1);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            gameObject.transform.position = gameObject.transform.position + new Vector3(0, 1, 0);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            gameObject.transform.position = gameObject.transform.position + new Vector3(0, -1, 0);
+        }*/
+    }
+}
